9eb23215e3e9d48dba738d18ef6a7d0ad92255e4
Muhammad <arief0039@gmail.com>
2025-01-19 21:30:01 +0700

    Update config.ts 5

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    RPC_ENDPOINT: "https://solana-mainnet.core.chainstack.com/958fa0ac350a1ef559481039febf671e",
+    RPC_ENDPOINT: "https://solana-rpc.publicnode.com",
     MAX_BATCH_SIZE: 1000,
     MAX_RETRIES: 15,
     INITIAL_RETRY_DELAY: 1000,
