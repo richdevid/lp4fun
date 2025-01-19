a6a007148dfe0480a8fe32599c881065135849ec
Muhammad <arief0039@gmail.com>
2025-01-19 20:38:26 +0700

    Update config.ts

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    RPC_ENDPOINT: "https://rpc-proxy.segfaultx0.workers.dev",
+    RPC_ENDPOINT: "https://weathered-nameless-valley.solana-mainnet.quiknode.pro/d563749eb5b84e1f819f8b6805f644a01479620c",
     MAX_BATCH_SIZE: 1000,
     MAX_RETRIES: 10,
     INITIAL_RETRY_DELAY: 1000,
