41e56c2a4b861531901cd566e43e846df14b8b84
Muhammad <arief0039@gmail.com>
2025-01-20 20:28:21 +0700

    Update config.ts

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    RPC_ENDPOINT: "https://lb.drpc.org/ogrpc?network=solana&dkey=AiVxMYzlTUBYiDXTeHDfyAYDFHlD1zER74waKuk0h5Qw",
+    RPC_ENDPOINT: "https://mainnet.helius-rpc.com/?api-key=1cd911e1-222e-47ba-a646-a203b7750ec9",
     MAX_BATCH_SIZE: 1000,
     MAX_RETRIES: 15,
     INITIAL_RETRY_DELAY: 1000,
