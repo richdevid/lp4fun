6327f04165f7948b6ff41d1f11ad2fac046bb7d6
Muhammad <arief0039@gmail.com>
2025-01-20 20:57:00 +0700

    Update config.ts

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    RPC_ENDPOINT: "https://solana-mainnet.api.syndica.io/api-key/2RGPs37rRGEMT7x3XWPssn2EoEUL6384f92W17MDiD2CBF3ncMraszfFMsyx3pqSE6vVWJu5AQa38f9J2ta4RxZPM2gi6HFXqTQ",
+    RPC_ENDPOINT: "https://rpc-proxy.segfaultx0.workers.dev",
     MAX_BATCH_SIZE: 1000,
     MAX_RETRIES: 15,
     INITIAL_RETRY_DELAY: 1000,
